8fa1609d10fc741bdfea92178b04f4a1bef48991
Mazen ALSAREM <ALSAREM@users.noreply.github.com>
2024-05-25 01:58:09 +0200

    prompt engineering local master

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -2,10 +2,11 @@ from openai import OpenAI
 
 from core.config import settings
 
+
 class aiService():
     def __init__(self):
-        self.client = OpenAI(api_key = settings.OPENAI_API_KEY)
-    
+        self.client = OpenAI(api_key=settings.OPENAI_API_KEY)
+
     def openai_response(self, prompt):
         response_obj = self.client.chat.completions.create(
             model="gpt-3.5-turbo",  # You can choose other engines as well
