a3a5c6fd7bed387c3029ac4ed86044d7eaf0cdb1
Jaydevsinh <jaydev.z@neuramonks.com>
2024-05-27 19:23:49 +0530

    update json files routing.

---
+++ backend/core/apps/database_creation_changes/routes.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter
+from core.apps.database_creation_changes import services
+from core.config import settings
+
+db_router = APIRouter(
+    prefix=str(settings.API_VERSION_STR),
+    tags=["create new database "]
+)
+
+@db_router.post('/update_features_json_file')
+def update_features_json_file(json_data):
+    return services.update_features_json_file(json_data)
+
+@db_router.post('/update_distributors_json_file')
+def update_distributors_json_file(json_data):
+    return services.update_distributors_json_file(json_data)
+
+@db_router.post('/update_market_json_file')
+def update_market_json_file(json_data):
+    return services.update_market_json_file(json_data)
+
+@db_router.post('/update_marketing_axis_json_file')
+def update_marketing_axis_json_file(json_data):
+    return services.update_marketing_axis_json_file(json_data)
+
+@db_router.post('/update_product_json_file')
+def update_product_json_file(json_data):
+    return services.update_product_json_file(json_data)
---
+++ backend/core/apps/database_creation_changes/utils.py
@@ -0,0 +1,66 @@
+import json
+
+def update_json_file_in_static(json_path, json_data):
+    with open(json_path, 'w') as fp:
+        json.dump(json_data, fp)
+
+def check_json_structure(json_str_data):
+    print(json_str_data)
+    try:
+        parsed_json = eval(json_str_data)
+        
+        print("parsed_json::: ", parsed_json)
+        print(type(parsed_json))
+        if isinstance(parsed_json, dict):
+            return parsed_json
+        else:
+            return False
+    except json.JSONDecodeError:
+        return False
+
+
+def check_distributor_json_structure(json_str_data):
+    distributor_original_key_list = sorted(["Id", "Label", "Title", "Description", "Main target", "Seo keywords", "Tone"])
+    
+    try:
+        parsed_json = eval(json_str_data)
+        print("~~ ~~ " * 20)
+        for key in parsed_json.keys():
+            print(parsed_json[key].keys())
+            if sorted(parsed_json[key].keys()) == distributor_original_key_list:
+                return True
+            else:
+                return False
+    except json.JSONDecodeError:
+        return False
+    
+    
+def check_market_json_structure(json_str_data):
+    market_original_key_list = sorted(["Id", "Title","Marketing_axis","Cultural_recommendations","Languages","SEO_keywords","Market_features"])
+    
+    try:
+        parsed_json = eval(json_str_data)
+        print("~~ ~~ " * 20)
+        for key in parsed_json.keys():
+            print(parsed_json[key].keys())
+            if sorted(parsed_json[key].keys()) == market_original_key_list:
+                return True
+            else:
+                return False
+    except json.JSONDecodeError:
+        return False
+    
+def check_product_json_structure(json_str_data):
+    product_original_key_list = sorted(["Title", "Description", "Features_1", "features_2", "features_3", "Url"])
+    
+    try:
+        parsed_json = eval(json_str_data)
+        print("~~ ~~ " * 20)
+        for key in parsed_json.keys():
+            print(parsed_json[key].keys())
+            if sorted(parsed_json[key].keys()) == product_original_key_list:
+                return True
+            else:
+                return False
+    except json.JSONDecodeError:
+        return False
